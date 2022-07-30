387b02072c99bc569ae1c47a9cc49ac9384a6a36
mohsen hegazy <hegazy@outlook.com>
2022-07-30 18:07:33 +0200

    Enhancement

--- app/services/cache.service.js
+++ app/services/cache.service.js
@@ -2,7 +2,7 @@ import { CacheModel } from "../models/index.js";
 import {getRandomValue} from '../helpers/index.js';
 
 export class CacheService {
-  
+  model = CacheModel;
   constructor() {
     if (CacheService._instance) {
       return CacheService._instance
@@ -11,31 +11,31 @@ export class CacheService {
   }
 
   async getAll(){
-    return await CacheModel.find({});
+    return await this.model.find({});
   }
   async getByKey({key}){
-    return await CacheModel.findOne({key});
+    return await this.model.findOne({key});
   }
   async create({key, value}){
-    return await CacheModel.findOneAndUpdate(
+    return await this.model.findOneAndUpdate(
       {key},
       {$set: {key, value: value || getRandomValue()}},
       {upsert: true, new: true}
     );
   }
   async update({key, value}){
-    return await CacheModel.findOneAndUpdate(
+    return await this.model.findOneAndUpdate(
       {key},
       {$set: {key, value: value || getRandomValue()}},
       {upsert: true, new: true}
     );
   }
   async deleteAll(){
-    await CacheModel.remove({});
+    await this.model.remove({});
     return {message: 'All data cleared'}
   }
   async deleteOne({key}){
-    await CacheModel.deleteOne({key})
+    await this.model.deleteOne({key})
     return {message: `record with key: ${key} deleted`}
   }
 }
