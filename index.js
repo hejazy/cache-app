e6b65bae96bb82ee025d87041e1a4f79b07317cb
mohsen hegazy <hegazy@outlook.com>
2022-07-30 14:46:29 +0200

    UPD: adding the app code

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 
 import express from 'express';
+import {DatabaseService, LoggerService} from './lib/index.js'
 const app = express();
 
 
 async function bootstrap() {
+  const cors = process.env.ALLOWED_DOMAINS && { origin: process.env.ALLOWED_DOMAINS.split(',') };
+  const port = process.env.PORT || 8080;
+  const loggerService = new LoggerService();
+  const databaseService = new DatabaseService();
+  app.use(express.json());
 
+  app.listen(port, () => {
+    loggerService.log({message: `App started at http://localhost:${port}`});
+  });
   
 }
 bootstrap();
